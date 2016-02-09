d6d25dac12fd3eeb807948438a002bd9c232fb8f
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-02-09 22:18:02 +0000

    Continued the a2e-frontend developments.

--- a2e/a2e-frontend/src/main/webapp/js/a2eApp.js
+++ a2e/a2e-frontend/src/main/webapp/js/a2eApp.js
@@ -1,7 +1,17 @@
 'use strict';
 
-var a2eApp = angular.module('a2eApp', []);
+/**
+ * Angular2Elastic module.
+ */
+var a2eApp = angular.module('a2eApp', [ 'ui.bootstrap' ]);
 
+// ===========
+// Controllers
+// ===========
+
+/**
+ * Main controller.
+ */
 a2eApp.controller('MainController', function() {
-	
-})
+
+});
