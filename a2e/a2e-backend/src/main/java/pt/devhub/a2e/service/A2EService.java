82b3c913074bb28121bdd916198971b76083adcd
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-03-26 01:08:19 +0000

    Added javadoc to back-end.

--- a2e/a2e-backend/src/main/java/pt/devhub/a2e/service/A2EService.java
+++ a2e/a2e-backend/src/main/java/pt/devhub/a2e/service/A2EService.java
@@ -2,9 +2,24 @@ package pt.devhub.a2e.service;
 
 import javax.ws.rs.core.Response;
 
+/**
+ * Interface containing the REST API declaration.
+ */
 public interface A2EService {
 
-	public String getFromElastic();
+	/**
+	 * Gets the data stored in Elastic database.
+	 * 
+	 * @return the data stored in Elastic database
+	 */
+	public Response getFromElastic();
 
+	/**
+	 * Persists a value on Elastic database.
+	 * 
+	 * @param valueToPersist
+	 *            the value to persist on Elastic database
+	 * @return the result of the value persistence on Elastic database
+	 */
 	public Response persistOnElastic(String valueToPersist);
 }
--- a2e/a2e-backend/src/main/java/pt/devhub/a2e/service/impl/A2EServiceImpl.java
+++ a2e/a2e-backend/src/main/java/pt/devhub/a2e/service/impl/A2EServiceImpl.java
@@ -7,28 +7,48 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.json.JSONObject;
 
 import pt.devhub.a2e.service.A2EService;
 
+/**
+ * Implementation of the REST service that allows to call Elastic API from
+ * AngularJS.
+ */
 @Path("/service")
 public class A2EServiceImpl implements A2EService {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@GET
 	@Path("/elastic/get")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Override
-	public String getFromElastic() {
-		// TODO Auto-generated method stub
-		return null;
+	public Response getFromElastic() {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("helloWorld", "Hello World!");
+
+		return Response.status(Status.OK).entity(jsonObject).build();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@POST
 	@Path("/elastic/post")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Override
 	public Response persistOnElastic(final String valueToPersist) {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("helloWorld", "Hello World!");
+		jsonObject.put("Received from server", valueToPersist);
+
+		return Response.status(Status.OK).entity(jsonObject).build();
 	}
 
 }
